36e2905b45f15b998b6825915bd7d1ce42df95a2
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-02-19 09:18:16 +0000

    adde get older

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 class Tree:
   height = 5
   leafs = 21461
-  age = 6
+  age = 10
   colortrumps = "brown"
   colorleafs = "green"
 
+  def __init__(self):
+    print("")
+
+  def get_older(self):
+    self.age += 1
 
 my_tree = Tree()
+
+print(my_tree.age)
+
+my_tree.get_older()
+
+print(my_tree.age)
+
+my_tree.get_older()
+
+print(my_tree.age)
+
+my_tree.get_older()
+
+print(my_tree.age)
+
+my_tree.get_older()
+
+print(my_tree.age)
+
+my_tree.get_older()
+
+print(my_tree.age)
